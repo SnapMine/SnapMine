f84044929c14993cd3ec0b4a27f6fcc5246c51e5
Nirbose <nirbose2@gmail.com>
2025-07-17 19:48:00 +0200

    feat: decode function and fix enum cases

--- src/Component/DataComponentType.php
+++ src/Component/DataComponentType.php
@@ -19,145 +19,169 @@ use Nirbose\PhpMcServ\Entity\Variant\TropicalFishVariant;
 use Nirbose\PhpMcServ\Inventory\ItemRarity;
 use Nirbose\PhpMcServ\Utils\DyeColor;
 
-enum DataComponentType: string
+enum DataComponentType
 {
     /**
      * Customizable data that doesn't fit any specific component.
      */
-    const CUSTOM_DATA = "";
-    const MAX_STACK_SIZE = "";
+    case CUSTOM_DATA;
+    case MAX_STACK_SIZE;
 
     /**
      * The maximum damage the item can take before breaking.
      */
-    const MAX_DAMAGE = "";
+    case MAX_DAMAGE;
 
     /**
      * The current damage of the item.
      */
-    const DAMAGE = "";
+    case DAMAGE;
 
-    const UNBREAKABLE = "";
+    case UNBREAKABLE;
 
     /**
      * Item's custom name.
      * Normally shown in italic, and changeable at an anvil.
      */
-    const CUSTOM_NAME = "";
+    case CUSTOM_NAME;
 
     /**
      * Override for the item's default name.
      * Shown when the item has no custom name.
      */
-    const ITEM_NAME = "";
-    const ITEM_MODEL = "";
-    const LORE = ItemLore::class;
-    const RARITY = ItemRarity::class;
-    const ENCHANTMENTS = "";
-    const CAN_PLACE_ON = BlockPredicates::class;
-    const CAN_BREAK = BlockPredicates::class;
-    const ATTRIBUTE_MODIFIERS = "";
-    const CUSTOM_MODEL_DATA = CustomModelData::class;
-    const TOOLTIP_DISPLAY = TooltipDisplay::class;
-    const REPAIR_COST = "";
+    case ITEM_NAME;
+    case ITEM_MODEL;
+    case LORE;
+    case RARITY;
+    case ENCHANTMENTS;
+    case CAN_PLACE_ON;
+    case CAN_BREAK;
+    case ATTRIBUTE_MODIFIERS;
+    case CUSTOM_MODEL_DATA;
+    case TOOLTIP_DISPLAY;
+    case REPAIR_COST;
 
     /**
      * Marks the item as non-interactive on the creative inventory (the first 5 rows of items).
      * This is used internally by the client on the paper icon in the saved hot-bars tab.
      */
-    const CREATIVE_SLOT_LOCK = ""; // Empty value (no fields)
-    const ENCHANTMENT_GLINT_OVERRIDE = "";
-    const INTANGIBLE_PROJECTILE = "";
-    const FOOD = FoodProperties::class;
-    const CONSUMABLE = "";
-    const USE_REMAINDER = "";
-    const USE_COOLDOWN = "";
-    const DAMAGE_RESISTANT = "";
-    const TOOL = "";
-    const WEAPON = "";
-    const ENCHANTABLE = "";
-    const EQUIPPABLE = "";
-    const REPAIRABLE = "";
+    case CREATIVE_SLOT_LOCK; // Empty value (no fields)
+    case ENCHANTMENT_GLINT_OVERRIDE;
+    case INTANGIBLE_PROJECTILE;
+    case FOOD;
+    case CONSUMABLE;
+    case USE_REMAINDER;
+    case USE_COOLDOWN;
+    case DAMAGE_RESISTANT;
+    case TOOL;
+    case WEAPON;
+    case ENCHANTABLE;
+    case EQUIPPABLE;
+    case REPAIRABLE;
 
     /**
      * Makes the item function like elytra.
      */
-    const GLIDER = ""; // Empty value (no fields)
-    const TOOLTIP_STYLE = "";
-    const DEATH_PROTECTION = "";
-    const BLOCKS_ATTACKS = "";
-    const STORED_ENCHANTMENTS = "";
+    case GLIDER; // Empty value (no fields)
+    case TOOLTIP_STYLE;
+    case DEATH_PROTECTION;
+    case BLOCKS_ATTACKS;
+    case STORED_ENCHANTMENTS;
 
     /**
      * Color of dyed leather armor.
      */
-    const DYED_COLOR = DyeItemColor::class;
+    case DYED_COLOR;
 
     /**
      * Color of the markings on the map item model.
      */
-    const MAP_COLOR = DyeItemColor::class;
-    const MAP_ID = "";
-    const MAP_DECORATIONS = "";
-    const MAP_POST_PRECESSING = "";
-    const CHARGED_PROJECTILES = "";
-    const BUNDLE_CONTENTS = "";
-    const POTION_CONTENTS = "";
-    const POTION_DURATION_SCALE = "";
-    const SUSPICIOUS_STEW_EFFECTS = "";
-    const WRITABLE_BOOK_CONTENT = "";
-    const WRITTEN_BOOK_CONTENT = "";
-    const TRIM = "";
-    const DEBUG_STICK_STATE = "";
-    const ENTITY_DATA = "";
-    const BUCKET_ENTITY_DATA = "";
-    const BLOCK_ENTITY_DATA = "";
-    const INSTRUMENT = "";
-    const PROVIDES_TRIM_MATERIAL = "";
-    const OMINOUS_BOTTLE_AMPLIFIER = "";
-    const JUCKBOX_PLAYABLE = "";
-    const PROVIDES_BANNER_PATTERNS = "";
-    const RECIPES = "";
-    const LODESTONE_TRACKER = "";
-    const FIREWORK_EXPLOSION = "";
-    const FIREWORKS = "";
-    const PROFILE = "";
-    const NOTE_BLOCK_SOUND = "";
-    const BANNER_PATTERNS = "";
+    case MAP_COLOR;
+    case MAP_ID;
+    case MAP_DECORATIONS;
+    case MAP_POST_PRECESSING;
+    case CHARGED_PROJECTILES;
+    case BUNDLE_CONTENTS;
+    case POTION_CONTENTS;
+    case POTION_DURATION_SCALE;
+    case SUSPICIOUS_STEW_EFFECTS;
+    case WRITABLE_BOOK_CONTENT;
+    case WRITTEN_BOOK_CONTENT;
+    case TRIM;
+    case DEBUG_STICK_STATE;
+    case ENTITY_DATA;
+    case BUCKET_ENTITY_DATA;
+    case BLOCK_ENTITY_DATA;
+    case INSTRUMENT;
+    case PROVIDES_TRIM_MATERIAL;
+    case OMINOUS_BOTTLE_AMPLIFIER;
+    case JUCKBOX_PLAYABLE;
+    case PROVIDES_BANNER_PATTERNS;
+    case RECIPES;
+    case LODESTONE_TRACKER;
+    case FIREWORK_EXPLOSION;
+    case FIREWORKS;
+    case PROFILE;
+    case NOTE_BLOCK_SOUND;
+    case BANNER_PATTERNS;
 
     /**
      * Base color of the banner applied to a shield.
      */
-    const BASE_COLOR = DyeColor::class;
-    const POT_DECORATIONS = "";
-    const CONTAINER = "";
-    const BLOCK_STATE = "";
-    const BEES = "";
-    const LOCK = "";
-    const CONTAINER_LOOT = "";
-    const BREAK_SOUND = "";
-    const VILLAGER_VARIANT = "";
-    const WOLF_VARIANT = "";
-    const WOLF_SOUND_VARIANT = "";
-    const WOLF_COLLAR = DyeColor::class;
-    const FOX_VARIANT = FoxVariant::class;
-    const SALMON_SIZE = SalmonVariant::class;
-    const PARROT_VARIANT = "";
-    const TROPICAL_FISH_PATTERN = TropicalFishVariant::class;
-    const TROPICAL_FISH_BASE_COLOR = DyeColor::class;
-    const TROPICAL_FISH_PATTERN_COLOR = DyeColor::class;
-    const MOOSHROOM_VARIANT = MooshroomVariant::class;
-    const RABBIT_VARIANT = RabbitVariant::class;
-    const PIG_VARIANT = "";
-    const COW_VARIANT = "";
-    const CHICKEN_VARIANT = "";
-    const FOG_VARIANT = "";
-    const HORSE_VARIANT = HorseVariant::class;
-    const PAINTING_VARIANT = "";
-    const LLAMA_VARIANT = LlamaVariant::class;
-    const AXOLOTL_VARIANT = AxolotlVariant::class;
-    const CAT_VARIANT = "";
-    const CAT_COLLAR = DyeColor::class;
-    const SHEEP_COLOR = DyeColor::class;
-    const SHULKER_COLOR = DyeColor::class;
+    case BASE_COLOR;
+    case POT_DECORATIONS;
+    case CONTAINER;
+    case BLOCK_STATE;
+    case BEES;
+    case LOCK;
+    case CONTAINER_LOOT;
+    case BREAK_SOUND;
+    case VILLAGER_VARIANT;
+    case WOLF_VARIANT;
+    case WOLF_SOUND_VARIANT;
+    case WOLF_COLLAR;
+    case FOX_VARIANT;
+    case SALMON_SIZE;
+    case PARROT_VARIANT;
+    case TROPICAL_FISH_PATTERN;
+    case TROPICAL_FISH_BASE_COLOR;
+    case TROPICAL_FISH_PATTERN_COLOR;
+    case MOOSHROOM_VARIANT;
+    case RABBIT_VARIANT;
+    case PIG_VARIANT;
+    case COW_VARIANT;
+    case CHICKEN_VARIANT;
+    case FOG_VARIANT;
+    case HORSE_VARIANT;
+    case PAINTING_VARIANT;
+    case LLAMA_VARIANT;
+    case AXOLOTL_VARIANT;
+    case CAT_VARIANT;
+    case CAT_COLLAR;
+    case SHEEP_COLOR;
+    case SHULKER_COLOR;
+
+    public function handlerClass(): ?string
+    {
+        return match ($this) {
+            self::LORE => ItemLore::class,
+            self::RARITY => ItemRarity::class,
+            self::CAN_PLACE_ON, self::CAN_BREAK => BlockPredicates::class,
+            self::CUSTOM_MODEL_DATA => CustomModelData::class,
+            self::TOOLTIP_DISPLAY => TooltipDisplay::class,
+            self::FOOD => FoodProperties::class,
+            self::DYED_COLOR, self::MAP_COLOR, self::TROPICAL_FISH_BASE_COLOR, self::TROPICAL_FISH_PATTERN_COLOR,
+            self::CAT_COLLAR, self::SHEEP_COLOR, self::SHULKER_COLOR, self::WOLF_COLLAR, self::BASE_COLOR
+            => DyeColor::class,
+            self::FOX_VARIANT => FoxVariant::class,
+            self::SALMON_SIZE => SalmonVariant::class,
+            self::TROPICAL_FISH_PATTERN => TropicalFishVariant::class,
+            self::MOOSHROOM_VARIANT => MooshroomVariant::class,
+            self::RABBIT_VARIANT => RabbitVariant::class,
+            self::HORSE_VARIANT => HorseVariant::class,
+            self::LLAMA_VARIANT => LlamaVariant::class,
+            self::AXOLOTL_VARIANT => AxolotlVariant::class,
+            default => null,
+        };
+    }
 }
--- src/Inventory/ItemStack.php
+++ src/Inventory/ItemStack.php
@@ -2,6 +2,9 @@
 
 namespace Nirbose\PhpMcServ\Inventory;
 
+use Nirbose\PhpMcServ\Component\DataComponentType;
+use Nirbose\PhpMcServ\Network\Serializer\PacketSerializer;
+
 class ItemStack
 {
     private int $id; // TODO: Replace by Material enum
@@ -69,5 +72,36 @@ class ItemStack
     public function getMaxStackSize(): int
     {
         return $this->components[1];
+    }
+
+    public static function decode(string $buffer): ItemStack
+    {
+        $offset = 0;
+        $s = new PacketSerializer();
+
+        $amount = $s->getVarInt($buffer, $offset);
+        $id = $s->getVarInt($buffer, $offset);
+        $item = new self($id, $amount);
+
+        $numberComponentsAdded = $s->getVarInt($buffer, $offset);
+        $numberComponentsRemoved = $s->getVarInt($buffer, $offset);
+
+        for ($i = 0; $i < $numberComponentsAdded; $i++) {
+            $index = $s->getVarInt($buffer, $offset);
+
+            $class = DataComponentType::cases()[$index]->handlerClass();
+
+            if (is_null($class)) {
+                continue;
+            }
+
+            if (enum_exists($class)) {
+                $s->getVarInt($buffer, $offset);
+            } else {
+                // TODO: DECODE
+            }
+        }
+
+        return $item;
     }
 }
