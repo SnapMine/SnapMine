b984ce16c3e229fae9669bd9c8d2ce4f262db9e4
Nirbose <nirbose2@gmail.com>
2025-08-10 01:05:20 +0200

    feat add sound

---
+++ src/Sound/SoundCategory.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Nirbose\PhpMcServ\Sound;
+
+enum SoundCategory: int
+{
+    case MASTER = 0;
+    case MUSIC = 1;
+    case RECORDS = 2;
+    case WEATHER = 3;
+    case BLOCKS = 4;
+    case HOSTILE = 5;
+    case NEUTRAL = 6;
+    case PLAYERS = 7;
+    case AMBIENT = 8;
+    case VOICE = 9;
+}
