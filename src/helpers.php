6e73c55904677de23804e9782206aaa24b408527
Nirbose <nirbose2@gmail.com>
2025-08-10 23:03:37 +0200

    remove useless function

--- src/helpers.php
+++ src/helpers.php
@@ -20,21 +20,3 @@ if (!function_exists('packet_dump')) {
         echo "  - Hex : $packetData\n";
     }
 }
-
-if (!function_exists("get_block_state_offset")) {
-    function get_block_state_offset(array $property_values, array $property_coefficients): int
-    {
-        $offset = 0;
-
-        if (!array_is_list($property_values)) {
-            ksort($property_values, SORT_STRING);
-            $property_values = array_values($property_values);
-        }
-
-        for ($i = 0; $i < count($property_values); $i++) {
-            $offset += $property_values[$i] * $property_coefficients[$i];
-        }
-
-        return $offset;
-    }
-}
