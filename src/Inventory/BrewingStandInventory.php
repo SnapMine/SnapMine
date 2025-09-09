2b96dd3bc92e1a75446f7e9b560afaf9d75b6b83
Tazzeuh <125388174+Tazzeuh@users.noreply.github.com>
2025-09-09 15:01:34 +0200

    Add OpenScreenPacket (#63)
    * - Add OpenScreenPacket
    - Modifying the UseItemOnPacket in combination of the new OpenScreenPacket.
    
    * + Add class to all inventory Type
    + Use InventoryType type instead of int in the OpenScreenPacket
    - Delete entity InventoryGUI

---
+++ src/Inventory/BrewingStandInventory.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace SnapMine\Inventory;
+
+class BrewingStandInventory extends Inventory
+{
+    const BOTTLE_SLOT = [0,1,2];
+    const INGREDIENT_SLOT = 3;
+    const FUEL_SLOT = 4;
+
+    public function __construct()
+    {
+        parent::__construct(InventoryType::BREWING_STAND);
+    }
+
+    public function getWindowId(): int
+    {
+        return 1;
+    }
+
+    public function getBottleSlot(int $BottleId): ItemStack
+    {
+        return $this->getItem($this::BOTTLE_SLOT[$BottleId]);
+    }
+    public function setBottleSlot(ItemStack $item, int $BottleId): void
+    {
+        $this->setItem($this::BOTTLE_SLOT[$BottleId], $item);
+    }
+    
+    public function getIngredient(ItemStack $item): void
+    {
+        $this->getItem($this::INGREDIENT_SLOT);
+    }
+    public function setIngredient(ItemStack $item): void
+    {
+        $this->setItem($this::INGREDIENT_SLOT, $item);
+    }
+
+    public function getFuel(ItemStack $item): void
+    {
+        $this->getItem($this::FUEL_SLOT);
+    }
+    public function setFuel(ItemStack $item): void
+    {
+        $this->setItem($this::FUEL_SLOT, $item);
+    }
+}
---
+++ src/Inventory/FurnaceInventory.php
@@ -0,0 +1,47 @@
+<?php 
+
+namespace SnapMine\Inventory;
+
+class FurnaceInventory extends Inventory
+{
+    const INGREDIENT_SLOT = 0;
+    const FUEL_SLOT = 1;
+    const OUTPUT_SLOT = 2;
+
+    public function __construct()
+    {
+        parent::__construct(InventoryType::FURNACE);
+    }
+
+    public function getWindowId(): int
+    {
+        return 1;
+    }
+
+    public function getIngredient(): ItemStack
+    {
+        return $this->getItem($this::INGREDIENT_SLOT);
+    }
+    public function setIngredient(ItemStack $item): void
+    {
+        $this->setItem($this::INGREDIENT_SLOT, $item);
+    }
+    
+    public function getFuel(ItemStack $item): void
+    {
+        $this->getItem($this::FUEL_SLOT);
+    }
+    public function setFuel(ItemStack $item): void
+    {
+        $this->setItem($this::FUEL_SLOT, $item);
+    }
+
+    public function getOutput(ItemStack $item): void
+    {
+        $this->getItem($this::OUTPUT_SLOT);
+    }
+    public function setOutput(ItemStack $item): void
+    {
+        $this->setItem($this::OUTPUT_SLOT, $item);
+    }
+}
